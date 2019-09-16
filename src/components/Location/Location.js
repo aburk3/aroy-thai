71fe61082f9ff34d3e8f80d2aa2be68937b6387f
aburk3 <aburk3@gmail.com>
2019-09-16 17:37:10 -0500

    Added dummy content for location

---
+++ src/components/Location/Location.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Location = props => {
+  return (
+    <>
+      <h2>
+        Hi
+      </h2>
+
+      <div>
+        Wow! What a great location!
+      </div>
+    </>
+  );
+};
+
+export default Location;
