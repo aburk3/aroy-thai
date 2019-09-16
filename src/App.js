3576aa4c02ecb59515452543c41f14329cfc192e
aburk3 <aburk3@gmail.com>
2019-09-16 17:12:54 -0500

    Modified App.js to have location route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar"
 import MenuCard from "./components/MenuCard/MenuCard"
+import Location from "./components/Location/Location"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" exact component={MenuCard} />
         {/* <Route path="/edit/:id" component={EditMenuItem} /> */}
         {/* <Route path="/menuitem" component={CreateMenuItem} /> */}
+        <Route path="/location" exact component={Location} />
       </div>
     </Router>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,44 +1,22 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import { FaAlignRight } from "react-icons/fa";
+import classes from "./Navbar.module.css";
 
 export default class Navbar extends Component {
-  state = {
-    isOpen: false
-  };
-
-  handleToggle = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
 
   render() {
     return (
-      <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <Link to="/">
-              {/* <img src={logo} alt="Arroy Thai" /> */}
-            </Link>
-            <button
-              type="button"
-              className="nav-btn"
-              onClick={this.handleToggle}
-            >
-              <FaAlignRight className="nav-icon" />
-            </button>
-          </div>
-          <ul
-            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
-          >
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/location">Location</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <Link to="/" className="navbar-brand">Menu</Link>
+      <div className="collpase navbar-collapse">
+      <ul className="navbar-nav mr-auto">
+        <li className="navbar-item">
+          <Link to="/location" className="nav-link">location</Link>
+        </li>
+      </ul>
+      </div>
+    </nav>
     );
   }
 }
