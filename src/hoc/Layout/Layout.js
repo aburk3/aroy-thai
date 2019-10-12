88cec7b3453c35f052fec9b5a1989aa149533a6b
aburk3 <aburk3@gmail.com>
2019-10-11 19:15:10 -0500

    Moved logo off of navbar and also added 'callable' phone number to nav bar

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
 import classes from "./Layout.module.css";
+import Logo from "../../components/Logo/Logo";
 
 class Layout extends Component {
   render() {
     return (
       <div>
+        <Logo />
         <main className={classes.Content}>{this.props.children}</main>
       </div>
     );
