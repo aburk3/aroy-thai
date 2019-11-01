49e935ca41f6da464c250aad029330c08813702d
aburk3 <aburke@flexera.com>
2019-11-01 09:46:53 -0500

    Added image of restaurant

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+
 import classes from "./LandingPage.module.css";
+import aroyOutside from "../../images/aroy-thai-outside.jpg";
 
 const LandingPage = props => {
   return (
@@ -10,18 +13,26 @@ const LandingPage = props => {
         </h2>
       </div>
 
-      <section>
-        <div class="container">
-
-          <ul>
-
-            <li>column </li>
-            <li>column </li>
-            <li>column </li>
-
-          </ul>
-        </div>
-      </section>
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+      <div className={classes.RestaurantLogo}>
+        <a
+          href="https://www.google.com/maps/dir/30.6855936,-88.0607232/aroy+thai+mobile+al/@30.6853044,-88.0609304,17z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x889a4e31e0df0617:0xea586a29df7bcbb4!2m2!1d-88.0569448!2d30.6857583"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={aroyOutside} alt="AroyThai" />
+        </a>
+      </div>
     </>
   );
 };
