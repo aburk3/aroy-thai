19226b23115e6f5fdc0de5acd51bfa746c2e102f
Alex Drozd <drozdster@gmail.com>
2019-11-07 00:30:11 -0600

    fixes path to .env in backend

--- backend/routes/menus.js
+++ backend/routes/menus.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-let Menu = require('../models/menu.model');
+const router = require("express").Router();
+let Menu = require("../models/menu.model");
 
-router.route('/').get((req, res) => {
-	Menu.find()
-		.then(menus => res.json(menus))
-		.catch(err => res.status(400).json('Error: ' + err));
+router.route("/").get((req, res) => {
+  Menu.find()
+    .then(menus => res.json(menus))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "../.env" });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,19 +10,25 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-	console.log("MongoDB database connection established successfully");
-})
 
-const menusRouter = require('./routes/menus');
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+const connection = mongoose.connection;
+connection.on("error", console.error.bind(console, "connection error:"));
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const menusRouter = require("./routes/menus");
 // const locationsRouter = require('./routes/locations');
 
-app.use('/menus', menusRouter);
+app.use("/menus", menusRouter);
 // app.use('/locations', locationsRouter);
 
 app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
